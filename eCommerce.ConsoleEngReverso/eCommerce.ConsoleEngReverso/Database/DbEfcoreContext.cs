dddc2af53644c71c39f9aaf1d7087234ae1a9eca
pedrohenriqueribei <pedrohenriqueribei@gmail.com>
2024-11-12 22:05:13 -0300

    Projeto Scaffold Engenharia Reversa - do banco de dados para o projeto C#

---
+++ eCommerce.ConsoleEngReverso/eCommerce.ConsoleEngReverso/Database/DbEfcoreContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using eCommerce.ConsoleEngReverso.Models;
+
+namespace eCommerce.ConsoleEngReverso.Database;
+
+public partial class DbEfcoreContext : DbContext
+{
+    public DbEfcoreContext()
+    {
+    }
+
+    public DbEfcoreContext(DbContextOptions<DbEfcoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contato> Contatos { get; set; }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<EnderecosEntrega> EnderecosEntregas { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DB_EFCore;Integrated Security=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contato>(entity =>
+        {
+            entity.HasIndex(e => e.UsuarioId, "IX_Contatos_UsuarioId").IsUnique();
+
+            entity.HasOne(d => d.Usuario).WithOne(p => p.Contato).HasForeignKey<Contato>(d => d.UsuarioId);
+        });
+
+        modelBuilder.Entity<Departamento>(entity =>
+        {
+            entity.HasMany(d => d.Usuarios).WithMany(p => p.Departamentos)
+                .UsingEntity<Dictionary<string, object>>(
+                    "DepartamentoUsuario",
+                    r => r.HasOne<Usuario>().WithMany().HasForeignKey("UsuariosId"),
+                    l => l.HasOne<Departamento>().WithMany().HasForeignKey("DepartamentosId"),
+                    j =>
+                    {
+                        j.HasKey("DepartamentosId", "UsuariosId");
+                        j.ToTable("DepartamentoUsuario");
+                        j.HasIndex(new[] { "UsuariosId" }, "IX_DepartamentoUsuario_UsuariosId");
+                    });
+        });
+
+        modelBuilder.Entity<EnderecosEntrega>(entity =>
+        {
+            entity.ToTable("EnderecosEntrega");
+
+            entity.HasIndex(e => e.UsuarioId, "IX_EnderecosEntrega_UsuarioId");
+
+            entity.Property(e => e.Cep).HasColumnName("CEP");
+
+            entity.HasOne(d => d.Usuario).WithMany(p => p.EnderecosEntregas).HasForeignKey(d => d.UsuarioId);
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.Property(e => e.Cpf).HasColumnName("CPF");
+            entity.Property(e => e.Rg).HasColumnName("RG");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ eCommerce.ConsoleEngReverso/eCommerce.ConsoleEngReverso/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using eCommerce.ConsoleEngReverso.Database;
+
+var db = new DbEfcoreContext();
+
+foreach(var usuario in db.Usuarios)
+{
+    Console.WriteLine(usuario.Nome);
+}
